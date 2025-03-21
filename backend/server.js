3b033d601095ec89c7dc9cd7a11959fd1c6c3e6e
Canberkkmn <canberkkaman@gmail.com>
2025-03-21 11:38:39 +0300

    add redis pub sub

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const http = require("http");
 const { Server } = require("socket.io");
+const { createClient } = require("redis");
+const { createAdapter } = require("@socket.io/redis-adapter");
 const app = require("./app");
 const config = require("./config");
 const { setupSocketHandlers } = require("./socketHandlers");
@@ -14,13 +16,21 @@ const io = new Server(server, {
     methods: config.CORS_METHODS,
   },
 });
-
-/**
- * Setup Socket.IO event handlers. Add (message, join, disconnect etc.)
- */
-setupSocketHandlers(io);
+const pubClient = createClient({ url: "redis://localhost:6379" });
+const subClient = pubClient.duplicate();
 
 // Start the server
 server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
+});
+
+Promise.all([pubClient.connect(), subClient.connect()]).then(() => {
+  io.adapter(createAdapter(pubClient, subClient));
+
+  // Setup Socket.IO event handlers. Add (message, join, disconnect etc.)
+  setupSocketHandlers(io);
+
+  server.listen(config.PORT, () => {
+    console.log(`Server running on port ${config.PORT}`);
+  });
 });
